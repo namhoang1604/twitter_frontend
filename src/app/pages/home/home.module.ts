d49a4c627964337afcc6897a8d151832bb65c733
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:32:49 +0700

    adjust to declare tweet detail component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ShareModule } from '@share/modules/share.module';
+import { TweetDetailComponent } from '@src/app/components/tweet-detail/tweet-detail.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TweetDetailComponent],
   imports: [CommonModule, HomeRoutingModule, ShareModule]
 })
 export class HomeModule {}
