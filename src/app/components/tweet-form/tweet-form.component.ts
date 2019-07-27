32d0bf59d8656da0ece5be79b67be26c714d1559
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:38:34 +0700

    Adjust to write function create tweet

--- src/app/components/tweet-form/tweet-form.component.ts
+++ src/app/components/tweet-form/tweet-form.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
+import { Tweet } from '@src/app/models/tweet';
+import { Store } from '@ngrx/store';
+import { RootStoreState } from '@src/app/root-store';
+import { BaseComponent } from '../base.component';
+import {
+  TweetStoreActions,
+  TweetStoreSelectors
+} from '@src/app/root-store/tweet-store';
+import { ActionTypes } from '@src/app/root-store/tweet-store/actions';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-tweet-form',
   templateUrl: './tweet-form.component.html',
   styleUrls: ['./tweet-form.component.scss']
 })
-export class TweetFormComponent implements OnInit {
+export class TweetFormComponent extends BaseComponent implements OnInit {
+  content = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    injector: Injector,
+    private store$: Store<RootStoreState.RootState>,
+    public dialogRef: MatDialogRef<TweetFormComponent>
+  ) {
+    super(injector);
   }
 
+  ngOnInit() {}
+
+  createTweet() {
+    this.store$.select(TweetStoreSelectors.selectType).subscribe(type => {
+      if (type === ActionTypes.CREATE_TWEETS_SUCCESS) {
+        this.dialogRef.close();
+      }
+    });
+    const tweet = new Tweet(this.content);
+    this.store$.dispatch(new TweetStoreActions.TweetsCreateAction({ tweet }));
+  }
 }
