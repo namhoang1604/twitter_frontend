ee70d771447747f274d4abeebe3d761d28953d61
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:37:15 +0700

    Adjust to generate anonymous user and select the user to show on UI

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { TweetFormComponent } from '../components/tweet-form/tweet-form.component';
+import { BaseComponent } from '../components/base.component';
+import { Store } from '@ngrx/store';
+import { RootStoreState } from '../root-store';
+import { UserStoreActions, UserStoreSelectors } from '../root-store/user-store';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
-export class PagesComponent implements OnInit {
-  constructor(public dialog: MatDialog) {}
+export class PagesComponent extends BaseComponent implements OnInit {
+  user$: any;
+  constructor(
+    injector: Injector,
+    private store$: Store<RootStoreState.RootState>,
+    public dialog: MatDialog
+  ) {
+    super(injector);
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user$ = this.store$.select(UserStoreSelectors.selectUser);
+    this.store$.dispatch(new UserStoreActions.GenerateUserAction());
+  }
 
   openDialogToCreateTweet(): void {
     const dialogRef = this.dialog.open(TweetFormComponent, {
