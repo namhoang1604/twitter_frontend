98fd8e4f314a31f6dcf10eea642e615d58b9d2a5
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:31:22 +0700

    adjust to change tweet model

--- src/app/models/tweet.ts
+++ src/app/models/tweet.ts
@@ -1,7 +1,10 @@
 export class Tweet {
   id: number;
   content: string;
-  retweets: number;
+  retweets: string[];
+  ownerID: string;
+  ownerName: string;
+  createdDate: string;
   constructor(content: string) {
     this.content = content;
   }
@@ -10,13 +13,18 @@ export class Tweet {
 export interface ITweet {
   id: number;
   content: string;
-  retweets: number;
+  retweets: string[];
+  owner_id: string;
+  owner_name: string;
+  inserted_at?: string;
 }
 
 export function convertToJson(tweet: Tweet): ITweet {
   const iTweet: ITweet = {
     id: tweet.id,
     content: tweet.content,
+    owner_id: tweet.ownerID,
+    owner_name: tweet.ownerName,
     retweets: tweet.retweets
   };
   return iTweet;
@@ -26,7 +34,10 @@ export function convertToClass(json: ITweet): Tweet {
   const tweet: Tweet = {
     id: json.id,
     content: json.content,
-    retweets: json.retweets
+    retweets: json.retweets,
+    ownerID: json.owner_id,
+    ownerName: json.owner_name,
+    createdDate: json.inserted_at
   };
   return tweet;
 }
