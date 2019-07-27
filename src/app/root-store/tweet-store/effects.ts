0da33b9821b7a87d9e342e3deab60d648b6f0768
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:28:08 +0700

    adjust to add create tweet effect and retweet effect

--- src/app/root-store/tweet-store/effects.ts
+++ src/app/root-store/tweet-store/effects.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { Observable, of as observableOf } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap, mergeMap } from 'rxjs/operators';
 import * as tweetAction from './actions';
 import { DataService } from '@src/app/services/data/data.service';
-import { Tweet, convertToClass } from '@src/app/models/tweet';
+import { Tweet, convertToClass, convertToJson } from '@src/app/models/tweet';
+import { RootStoreState } from '..';
+import { UserStoreSelectors } from '../user-store';
 
 @Injectable()
 export class TweetStoreEffects {
-  constructor(private dataService: DataService, private actions$: Actions) {}
+  constructor(
+    private dataService: DataService,
+    private actions$: Actions,
+    private store$: Store<RootStoreState.RootState>
+  ) {}
 
   @Effect()
   tweetRequestEffect$: Observable<Action> = this.actions$.pipe(
@@ -17,9 +23,11 @@ export class TweetStoreEffects {
       tweetAction.ActionTypes.TWEETS_REQUEST
     ),
     switchMap(action =>
-      this.dataService.get('tweet').pipe(
-        map(tweets => {
-          const listTweet: Tweet[] = tweets.map(tweet => convertToClass(tweet));
+      this.dataService.get('tweets').pipe(
+        map(response => {
+          const listTweet: Tweet[] = response.data.map(tweet =>
+            convertToClass(tweet)
+          );
           return new tweetAction.TweetsSuccessAction({
             tweets: listTweet
           });
@@ -29,5 +37,63 @@ export class TweetStoreEffects {
         )
       )
     )
+  );
+
+  @Effect()
+  tweetCreateEffect$: Observable<Action> = this.actions$.pipe(
+    ofType<tweetAction.TweetsCreateAction>(
+      tweetAction.ActionTypes.CREATE_TWEETS_REQUEST
+    ),
+    switchMap(action => {
+      return this.store$.select(UserStoreSelectors.selectUser).pipe(
+        mergeMap(userState => {
+          const tweetJson = convertToJson({
+            ...action.payLoad.tweet,
+            ownerID: userState.userID,
+            ownerName: userState.name
+          });
+          return this.dataService.post('tweets', { tweet: tweetJson }).pipe(
+            map(response => {
+              const tweet: Tweet = convertToClass(response.data);
+              return new tweetAction.TweetsCreateSuccessAction({ tweet });
+            }),
+            catchError(error =>
+              observableOf(new tweetAction.TweetsFailureAction({ error }))
+            )
+          );
+        })
+      );
+    })
+  );
+
+  @Effect()
+  tweetRetweetEffect$: Observable<Action> = this.actions$.pipe(
+    ofType<tweetAction.TweetsRetweetAction>(
+      tweetAction.ActionTypes.RETWEET_TWEETS_REQUEST
+    ),
+    switchMap(action => {
+      return this.store$.select(UserStoreSelectors.selectUser).pipe(
+        mergeMap(userState => {
+          return this.dataService
+            .post(`tweets/${action.payLoad.id}/retweet`, {
+              user_id: userState.userID
+            })
+            .pipe(
+              map(response => {
+                const tweet: Tweet = convertToClass(response.data);
+                return new tweetAction.TweetsRetweetSuccessAction({
+                  id: action.payLoad.id,
+                  changes: { ...tweet }
+                });
+              }),
+              catchError(error =>
+                observableOf(
+                  new tweetAction.TweetsRetweetFailureAction({ error })
+                )
+              )
+            );
+        })
+      );
+    })
   );
 }
