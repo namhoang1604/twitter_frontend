4dfd30b194ef170e54f9ff9a58e2c0f5adfff913
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-27 22:24:37 +0700

    adjust to add user state

--- src/app/root-store/root-state.ts
+++ src/app/root-store/root-state.ts
@@ -1,5 +1,7 @@
 import { TweetStoreState } from './tweet-store';
+import { UserStoreState } from './user-store';
 
 export interface RootState {
   Tweet: TweetStoreState.TweetState;
+  User: UserStoreState.UserState;
 }
