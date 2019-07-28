4e29dd29bdf39e99fab6e00d6fe788d16a692849
namhoang1604 <hoangxuannam160493@gmail.com>
2019-07-28 21:28:02 +0700

    Adjust to join to every tweets to listen change such as retweets

--- src/app/components/tweet-detail/tweet-detail.component.ts
+++ src/app/components/tweet-detail/tweet-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Injector } from '@angular/core';
+import { Component, OnInit, Input, Injector, OnDestroy } from '@angular/core';
 import { Tweet } from '@src/app/models/tweet';
 import { BaseComponent } from '../base.component';
 import { Store } from '@ngrx/store';
@@ -6,19 +6,23 @@ import { RootStoreState } from '@src/app/root-store';
 import { TweetStoreActions } from '@src/app/root-store/tweet-store';
 import { UserStoreSelectors } from '@src/app/root-store/user-store';
 import { UserState } from '@src/app/root-store/user-store/state';
+import { PhoenixChannelService } from '@src/app/services/phoenix-channel/phoenix-channel.service';
 
 @Component({
   selector: 'app-tweet-detail',
   templateUrl: './tweet-detail.component.html',
   styleUrls: ['./tweet-detail.component.scss']
 })
-export class TweetDetailComponent extends BaseComponent implements OnInit {
+export class TweetDetailComponent extends BaseComponent
+  implements OnInit, OnDestroy {
   @Input() tweet: Tweet;
   user: UserState;
+  channel: any;
 
   constructor(
     injector: Injector,
-    private store$: Store<RootStoreState.RootState>
+    private store$: Store<RootStoreState.RootState>,
+    private phoenixChannel: PhoenixChannelService
   ) {
     super(injector);
   }
@@ -27,6 +31,20 @@ export class TweetDetailComponent extends BaseComponent implements OnInit {
     this.store$
       .select(UserStoreSelectors.selectUser)
       .subscribe(user => (this.user = user));
+
+    this.channel = this.phoenixChannel.joinTopic(`tweets:${this.tweet.id}`);
+    this.channel.on('update_tweets', response =>
+      this.store$.dispatch(
+        new TweetStoreActions.TweetsUpdateTweetFromChannelAction({
+          tweet: response.data
+        })
+      )
+    );
+    this.channel.join();
+  }
+
+  ngOnDestroy() {
+    this.channel.leave();
   }
 
   isRetweeted(): boolean {
